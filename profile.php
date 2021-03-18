3fbdb9e22f7ba609729e6f45832c5d3a61f1134a
tbinkasem <tbinkasem@gmail.com>
2021-03-18 10:15:10 +0700

    ส่งข้อมูลครั้งที่ 2
    ไฟล์ profile.php profile.jpg profile.css

---
+++ profile.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Profile Card</title>
+      <link rel="stylesheet" type="text/css" href="css/profile.css">
+   </head>
+   <body>
+      <div class="card-container">
+         <div class="upper-container">
+            <div class="image-container">
+               <img src="images/profile.jpg" />
+            </div>
+         </div>
+         <div class="lower-container">
+            <div>
+               <h3>Teera Binkasem</h3>
+               <h4>Front-end Developer</h4>
+            </div>
+            <div>
+               <p>
+                   Email : tbinkasem@gmail.com
+               </p>
+            </div>
+            <br>
+            <div>
+               <a href="data.php" class="btn">เปิดดูข้อมูลส่วนตัว</a>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
