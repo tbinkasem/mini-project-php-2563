8f7c9408da590d63e8961344405a2c30423db11f
tbinkasem <tbinkasem@gmail.com>
2021-03-18 15:49:35 +0700

    ส่งไฟล์ครั้งที่ 5
    data.css
    data.php
    index.html
    data.js
    register.html

--- data.php
+++ data.php
@@ -7,35 +7,96 @@
 	$fullname = $_SESSION["Fullname"];
    $photo = $_SESSION["photo"];
 
+   $strSQL1 = "SELECT * FROM member WHERE email = '".$email."' ";
+	$objQuery1 = mysqli_query($conn, $strSQL1);
+   $result1 = mysqli_fetch_assoc($objQuery1);
+
+   $strSQL2 = "SELECT * FROM study WHERE email = '".$email."' ";
+	$objQuery2 = mysqli_query($conn, $strSQL2);
+   //$result2 = mysqli_fetch_assoc($objQuery2);
+
 ?>
 <!DOCTYPE html>
 <html>
    <head>
-      <title>Profile Card</title>
-      <link rel="stylesheet" type="text/css" href="css/profile.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="css/data.css">
    </head>
    <body>
-      <div class="card-container">
-         <div class="upper-container">
-            <div class="image-container">
-               <img src="images/<?php echo $photo; ?>" />
-            </div>
-         </div>
-         <div class="lower-container">
-            <div>
-               <h3><?php  echo $fullname; ?></h3>
-               <h4>Front-end Developer</h4>
-            </div>
-            <div>
-               <p>
-                   Email : <?php echo $email; ?>
-               </p>
-            </div>
-            <br>
-            <div>
-               <a href="data.php" class="btn">เปิดดูข้อมูลส่วนตัว</a>
-            </div>
-         </div>
+
+      <h2>ข้อมูลส่วนตัว [<?php echo $fullname; ?>]</h2>
+      <a href="profile.php" class="btnProfile">กลับไป Profile</a>
+      |
+      <a href="add_data.php" class="btnAddData">เพิ่มข้อมูลการเรียน</a>
+      <br><br>
+      <div class="tab">
+         <button class="tablinks" onclick="openData(event, 'account')">ข้อมูลบัญชีการใช้งาน</button>
+         <button class="tablinks" onclick="openData(event, 'study')">ข้อมูลการเรียน</button>
       </div>
+
+      <div id="account" class="tabcontent">
+         <h3>ข้อมูลบัญชีการใช้งาน</h3>
+         <table>
+            <tr>
+               <th>Full Name :</th>
+               <td>
+                  <?php echo $result1['fullname']; ?>
+               </td>
+            </tr>
+            <tr>
+               <th>Email :</th>
+               <td>
+                  <?php echo $result1['email']; ?>
+               </td>
+            </tr>
+            <tr>
+               <th>Password :</th>
+               <td>
+                  <?php echo $result1['password']; ?>
+               </td>
+            </tr>
+            <tr>
+               <th>Photo :</th>
+               <td>
+                  <?php echo $result1['photo']; ?>
+               </td>
+            </tr>
+            <tr>
+               <th colspan="2">
+                  <a href="edit_data.php" class="btnEdit">แก้ไขข้อมูล</a>
+            </tr>
+         </table>
+      </div>
+
+      <div id="study" class="tabcontent">
+         <h3>ข้อมูลการเรียน</h3>
+         <table class="data">
+            <tr>
+               <th class="data">รหัสวิชา</th>
+               <th class="data">ชื่อวิชา</th>
+               <th class="data">หน่วยกิต</th>
+               <th class="data">ผลการเรียน</th>
+            </tr>
+            <?php
+               while($result2 = mysqli_fetch_array($objQuery2, MYSQLI_ASSOC)){
+            ?>
+            <tr>
+               <td class="data1">
+                  <?php echo $result2['idsub']; ?>
+               </td>
+               <td>
+                  <?php echo $result2['namesub']; ?>
+               </td>
+               <td class="data1">
+                  <?php echo $result2['credit']; ?>
+               </td>
+               <td class="data1">
+                  <?php echo $result2['grade']; ?>
+               </td>
+            </tr>
+            <?php } ?>
+         </table>
+      </div>
+      <script src="js/data.js"></script>
    </body>
-</html>
+</html> 
---
+++ js/data.js
@@ -0,0 +1,13 @@
+function openData(evt, dataMenu) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(dataMenu).style.display = "block";
+    evt.currentTarget.className += "active";
+  }
